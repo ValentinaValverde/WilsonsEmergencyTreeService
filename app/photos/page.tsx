d72882e43f8c0265e388610c8957904ff07d4c48
Valentina Valverde <vvgreenville@gmail.com>
2024-08-11 15:29:38 -0400

    Wrapped up photos page (mobile)

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -75,187 +75,310 @@ export default function page() {
 
   return (
     <>
-      {isEnlarged ? (
-        <div
-          style={{
-            width: '100%',
-            height: '100vh',
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-            zIndex: 100,
-            position: 'fixed',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+      {isMobile ? (
+        <>
+          {isEnlarged ? (
+            <div
+              style={{
+                width: '100%',
+                height: '100vh',
+                backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                zIndex: 100,
+                position: 'fixed',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Image
+                  src={imageData[currentImageIndex]}
+                  alt="Image"
+                  width={500}
+                  height={500}
+                  style={{
+                    borderRadius: 10,
+                    objectFit: 'cover',
+                    maxWidth: '90%',
+                    marginLeft: 10,
+                    marginRight: 10,
+                  }}
+                />
+              </div>
+              <div style={{ height: 25 }} />
+
+              <div>
+                <Image
+                  src={LeftArrow}
+                  alt="Left Arrow"
+                  width={24}
+                  height={24}
+                  onClick={() => {
+                    handlePrevImage();
+                  }}
+                  style={{ marginRight: 20 }}
+                />
+                <Image
+                  src={RightArrow}
+                  alt="Right Arrow"
+                  width={24}
+                  height={24}
+                  onClick={() => {
+                    handleNextImage();
+                  }}
+                />
+              </div>
+              <div style={{ height: 25 }} />
+
+              <button
+                onClick={() => {
+                  setIsEnlarged(false);
+                }}
+              >
+                Exit
+              </button>
+            </div>
+          ) : null}
+
           <div
             style={{
+              height: '50vh',
               display: 'flex',
+              flexDirection: 'column',
               justifyContent: 'center',
               alignItems: 'center',
             }}
           >
             <Image
-              src={LeftArrow}
-              alt="Left Arrow"
-              width={24}
-              height={24}
-              onClick={() => {
-                handlePrevImage();
-              }}
-              style={{ cursor: 'pointer' }}
-            />
-
-            <Image
-              src={imageData[currentImageIndex]}
-              alt="Image"
-              width={500}
-              height={500}
+              src={BackgroundImage}
+              alt="Wilson's Emergency Tree Service Background"
+              layout="fill"
+              objectFit="cover"
               style={{
-                borderRadius: 10,
-                objectFit: 'cover',
-                maxWidth: '90%',
-                marginLeft: 10,
-                marginRight: 10,
+                filter: 'brightness(60%)',
+                zIndex: -1,
+                maskImage: 'linear-gradient(rgba(0, 0, 0, 1) 80%, transparent)',
               }}
             />
-
-            <Image
-              src={RightArrow}
-              alt="Right Arrow"
-              width={24}
-              height={24}
-              onClick={() => {
-                handleNextImage();
-              }}
-              style={{ cursor: 'pointer' }}
-            />
+            <h2
+              className={styles.center_text}
+              style={{ color: 'white', marginTop: 50 }}
+            >
+              Photos
+            </h2>
+            <p style={{ color: 'white' }}>Lorem ipsum dolor sit amet.</p>
           </div>
-          <div style={{ height: 25 }} />
-          <button
-            onClick={() => {
-              setIsEnlarged(false);
+
+          <div
+            style={{
+              padding: 20,
+              backgroundColor: 'white',
+              background:
+                'linear-gradient(0deg, rgba(255,255,255,1) 90%, rgba(255,255,255,0) 100%)',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap',
             }}
           >
-            Exit
-          </button>
-        </div>
-      ) : null}
+            {imageData.map((item, i) => {
+              return (
+                <div key={i} className="image-container">
+                  <Image
+                    src={item}
+                    alt={'Gallery Image'}
+                    width={150}
+                    height={150}
+                    style={{
+                      borderRadius: 10,
+                      backgroundColor: 'black',
+                      margin: 5,
+                      objectFit: 'cover',
+                      cursor: 'pointer',
+                    }}
+                    onClick={() => {
+                      setIsEnlarged(true);
+                      console.log(isEnlarged);
 
-      <div
-        style={{
-          height: '50vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Image
-          src={BackgroundImage}
-          alt="Wilson's Emergency Tree Service Background"
-          layout="fill"
-          objectFit="cover"
-          style={{
-            filter: 'brightness(60%)',
-            zIndex: -1,
-            maskImage: 'linear-gradient(rgba(0, 0, 0, 1) 80%, transparent)',
-          }}
-        />
-        <h2
-          className={styles.center_text}
-          style={{ color: 'white', marginTop: 50 }}
-        >
-          Photos
-        </h2>
-        <p style={{ color: 'white' }}>Lorem ipsum dolor sit amet.</p>
-      </div>
-
-      <div
-        style={{
-          backgroundColor: 'white',
-          margin: '-50px 50px',
-          padding: 50,
-          paddingBottom: 100,
-          borderRadius: 10,
-
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexWrap: 'wrap',
-        }}
-      >
-        {imageData.map((item, i) => {
-          return (
-            <div key={i} className="image-container">
-              {isDesktop && (
+                      setCurrentImageIndex(i);
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <>
+          {isEnlarged ? (
+            <div
+              style={{
+                width: '100%',
+                height: '100vh',
+                backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                zIndex: 100,
+                position: 'fixed',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
                 <Image
-                  src={item}
-                  alt={'Gallery Image'}
-                  width={300}
-                  height={300}
+                  src={LeftArrow}
+                  alt="Left Arrow"
+                  width={24}
+                  height={24}
+                  onClick={() => {
+                    handlePrevImage();
+                  }}
+                  style={{ cursor: 'pointer' }}
+                />
+
+                <Image
+                  src={imageData[currentImageIndex]}
+                  alt="Image"
+                  width={500}
+                  height={500}
                   style={{
                     borderRadius: 10,
-                    backgroundColor: 'black',
-                    margin: 5,
                     objectFit: 'cover',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => {
-                    setIsEnlarged(true);
-                    console.log(isEnlarged);
-
-                    setCurrentImageIndex(i);
+                    maxWidth: '90%',
+                    marginLeft: 10,
+                    marginRight: 10,
                   }}
                 />
-              )}
-              {isTablet && (
+
                 <Image
-                  src={item}
-                  alt={'Gallery Image'}
-                  width={250}
-                  height={250}
-                  style={{
-                    borderRadius: 10,
-                    backgroundColor: 'black',
-                    margin: 5,
-                    objectFit: 'cover',
-                    cursor: 'pointer',
-                  }}
+                  src={RightArrow}
+                  alt="Right Arrow"
+                  width={24}
+                  height={24}
                   onClick={() => {
-                    setIsEnlarged(true);
-                    console.log(isEnlarged);
-
-                    setCurrentImageIndex(i);
+                    handleNextImage();
                   }}
+                  style={{ cursor: 'pointer' }}
                 />
-              )}
-              {/* {isMobile && (
-                <Image
-                  src={item}
-                  alt={'Gallery Image'}
-                  width={150}
-                  height={150}
-                  style={{
-                    borderRadius: 10,
-                    backgroundColor: 'black',
-                    margin: 5,
-                    objectFit: 'cover',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => {
-                    setIsEnlarged(true);
-                    console.log(isEnlarged);
-
-                    setCurrentImageIndex(i);
-                  }}
-                />
-              )} */}
+              </div>
+              <div style={{ height: 25 }} />
+              <button
+                onClick={() => {
+                  setIsEnlarged(false);
+                }}
+              >
+                Exit
+              </button>
             </div>
-          );
-        })}
-      </div>
+          ) : null}
+
+          <div
+            style={{
+              height: '50vh',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              src={BackgroundImage}
+              alt="Wilson's Emergency Tree Service Background"
+              layout="fill"
+              objectFit="cover"
+              style={{
+                filter: 'brightness(60%)',
+                zIndex: -1,
+                maskImage: 'linear-gradient(rgba(0, 0, 0, 1) 80%, transparent)',
+              }}
+            />
+            <h2
+              className={styles.center_text}
+              style={{ color: 'white', marginTop: 50 }}
+            >
+              Photos
+            </h2>
+            <p style={{ color: 'white' }}>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div
+            style={{
+              backgroundColor: 'white',
+              margin: '-50px 50px',
+              padding: 50,
+              paddingBottom: 100,
+              borderRadius: 10,
+
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}
+          >
+            {imageData.map((item, i) => {
+              return (
+                <div key={i} className="image-container">
+                  {isDesktop && (
+                    <Image
+                      src={item}
+                      alt={'Gallery Image'}
+                      width={300}
+                      height={300}
+                      style={{
+                        borderRadius: 10,
+                        backgroundColor: 'black',
+                        margin: 5,
+                        objectFit: 'cover',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => {
+                        setIsEnlarged(true);
+                        console.log(isEnlarged);
+
+                        setCurrentImageIndex(i);
+                      }}
+                    />
+                  )}
+                  {isTablet && (
+                    <Image
+                      src={item}
+                      alt={'Gallery Image'}
+                      width={250}
+                      height={250}
+                      style={{
+                        borderRadius: 10,
+                        backgroundColor: 'black',
+                        margin: 5,
+                        objectFit: 'cover',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => {
+                        setIsEnlarged(true);
+                        console.log(isEnlarged);
+
+                        setCurrentImageIndex(i);
+                      }}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 }

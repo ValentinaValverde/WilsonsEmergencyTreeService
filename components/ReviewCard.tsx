ee4b0d90a61426271f5875976dcd439ef2fb5bb4
Valentina Valverde <vvgreenville@gmail.com>
2024-08-11 15:33:08 -0400

    Finished review page (mobile view)

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -129,9 +129,8 @@ export default function ReviewCard({
             padding: 20,
             borderRadius: 10,
             width: '100%',
-            height: 300,
+            // height: 300,
             margin: 10,
-
             boxShadow:
               'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
           }}
